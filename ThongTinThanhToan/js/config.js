95eb12a99922ac6a834c1b18fc3a77eacb7c09f6
Bùi Quang Khải <111097906+buiquangkhaiuser@users.noreply.github.com>
2023-02-04 12:36:10 +0700

    Add files via upload

---
+++ ThongTinThanhToan/js/config.js
@@ -0,0 +1,18 @@
+const USER = {
+    banks: [{
+            name: "MB BANK",
+            logo: "./images/mbbank.png",
+            number: "0784399599999 ",
+            owner: "NGUYEN HO NGUYEN CHUONG"
+        },
+      
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0352876128",
+            owner: "bui quang khai",
+            fastTransfer: true
+        },
+       
+    ]
+};
---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'BÙI QUANG KHẢI',
+        'favicon' : 'anhchinh.jpg',
+        'notice': 'BUI QUANG KHAI 2008'
+    },
+    'avatar-image' : 'anhchinh.jpg',
+    'name' : 'Bùi Quang Khải ', 
+    'is_tick' : true,
+    'skills' : [
+        'DDoS Attack ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'anhchinh.jpg',
+            'content' : 'LIÊN HỆ ADMIN',
+            'author' : 'Bùi Quang Khải',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Bùi Quang Khải',
+            'value' : 'https://xbinzh.ml/PayMent/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@buiquangkhai.user4' ,
+            'value' : 'https://www.facebook.com/buiquangkhai.user4/',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Bùi Quang Khải',
+            'value' : 'https://zalo.me/0352876128',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : '0352.876.128',
+            'value' : 'tel:0352876128',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'quangkhaicontactcopyright@gmail.com',
+            'value' : 'mailto:quangkhaicontactcopyright@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
